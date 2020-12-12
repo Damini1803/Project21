9fbf557a9f73c58d23fa1cf64fe2efe3d619eab2
Damini1803 <74255062+Damini1803@users.noreply.github.com>
2020-12-12 23:26:06 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,57 @@
+var bullet, wall;
+var speed, weight, thickness;
+
+function setup() {
+  createCanvas(1600,400);
+
+  speed = random(223,321);
+  weight= random(30,52);
+  thickness = random(22,83);
+
+  bullet = createSprite(50, 200, 100, 30);
+  bullet.shapeColor="white";
+
+  wall = createSprite(1300,200,thickness,height/2);
+  wall.shapeColor=color(80,80,80);
+  
+
+  bullet.velocityX = speed;
+}
+
+function draw() {
+  background("black");  
+
+  if(hasCollided(bullet, wall))
+  {
+  	bullet.velocityX=0;
+  	var damage=0.5 * weight * speed* speed/(thickness *thickness *thickness);
+
+  	
+	if(damage>10)
+	{
+		wall.shapeColor=color(255,0,0);
+		
+	}
+
+	
+
+	if(damage<10)
+	{
+		wall.shapeColor=color(0,255,0);
+	}
+	
+  }
+
+  drawSprites();
+}
+
+function hasCollided(lbullet, lwall)
+{
+	bulletRightEdge=lbullet.x +lbullet.width;
+	wallLeftEdge=lwall.x;
+	if (bulletRightEdge>=wallLeftEdge)
+	{
+		return true
+	}
+	return false;
+}
